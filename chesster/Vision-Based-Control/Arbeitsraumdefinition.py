7d3d597bf751ddfd5ee0aa3eea88ce57ebaebdd0
Thorben Pultke <thorben.pultke@gmx.de>
2021-12-04 17:19:51 +0100

    Hinzufügen des Arbeitsraum-Skripts

---
+++ chesster/Vision-Based-Control/Arbeitsraumdefinition.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Author: Thorben Pultke
+Contact: thorben.pultke@gmx.de
+Project: CHESSter
+Summary: Script to define the currently interesting workspace of the used robotic arm for training data generation. 
+Take care when running this script as every point in the defined 3D-Cubus can be accessed by randomized movements.
+"""
+
+from logging import exception
+import tkinter as tk
+import numpy as np
+import urx as urx
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg #Lib für die Einbindung von Matplotlib Diagrammen in Tkinter
+from mpl_toolkits.mplot3d import Axes3D #Import von 3D-Diagrammen
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection #3D Objekte für die Darstellung von Polygonen
+from matplotlib.figure import Figure #Generelle Lib für Matplotlib Diagrammen
+import threading as th 
+
+i=0
+
+class GUI:
+    def __init__(self,tk_object):  
+        self.gui = tk_object 
+        self.gui.title("Arbeitsraum Definition")
+        self.Coords = []
+        self.Points = [[6, 0, 2], [6, 8, 2], [0, 0, 2], [0, 8, 2], [6, 0, 6], [6, 8, 6], [0, 0, 6], [0, 8, 6]]
+        self.RPoints = [[9, 4, 0],[9, 4, 4],[7, 4, 6.5],[5, 4, 6],[3, 4, 4]]
+        container1 = tk.Frame(self.gui, relief=tk.RAISED) #Container 1 enthält alle in der GUI links angelegte Elemente (Einstellungen, etc.)
+        container2 = tk.Frame(self.gui, relief=tk.RIDGE) #Container 2 enthält alle Diagramme
+        container1.pack(side=tk.LEFT)
+        container2.pack(side=tk.LEFT)
+        self.L1 = tk.Label(container1, text="ACHTUNG")
+        self.L2 = tk.Label(container1, text="""Dieses Skript wird dich durch die Arbeitsraumdefinition führen. 
+Bitte folge den Schritten und Abbildungen auf dem Diagramm, 
+damit alles reibungslos läuft.
+
+Drücke Start zum Beginnen""")
+        self.L1.pack(pady=5)
+        self.L2.pack()
+        self.L3 = tk.Label(container1, text="Nicht verbunden...", bg='#ba3c3c', fg='white')
+        self.L3.pack(side=tk.RIGHT)
+        self.Ctrl = tk.Button(container1, text="Start", command=self.ProceedStep)
+        self.Ctrl.pack(pady=5)
+
+        self.fig = Figure(figsize=(9, 9))
+        self.ax = self.fig.add_subplot(1, 1, 1, projection='3d')
+        self.ax.set_xlabel('x')
+        self.ax.set_ylabel('y')
+        self.ax.set_zlabel('z')
+        self.ax.set_xlim3d([-2, 10])
+        self.ax.set_ylim3d([-2, 10])
+        self.ax.set_zlim3d([0, 12])
+        self.tk_plot = FigureCanvasTkAgg(self.fig, container2)
+        self.tk_plot.get_tk_widget().pack()
+    
+    def ProceedStep(self):
+        global i
+
+        if (i==0):
+            try:
+                #self.Rob = urx.Robot("XXX.XXX.XXX.XXX")
+                print('Test')
+            except Exception:
+                self.L1.config(text="FEHLER")
+                self.L2.config(text="Roboter konnte nicht verbunden werden. Überprüfen und neustarten...", fg='red')
+
+            else:
+                #if(self.Rob.is_running()):
+                if(True):
+                    self.L3.config(text="Verbunden.", bg='#3da872', fg='white')
+                    self.L1.config(text="Kalibrierung...")
+                    self.Ctrl.config(text="Weiter")
+                    self.L2.config(text="""Bewege den Roboterarm zum ersten Eckpunkt (siehe Diagramm)
+und drücke Weiter.""")
+                    i=i+1
+                    self.DrawPoints(0, 1)
+
+                    for j in range(5):
+                        color='green'
+                        self.ax.scatter3D(self.RPoints[j][0],self.RPoints[j][1],self.RPoints[j][2], c=color)
+                    for j in range(4):
+                        color='black'
+                        self.ax.plot3D((self.RPoints[j][0], self.RPoints[j+1][0]),(self.RPoints[j][1], self.RPoints[j+1][1]),(self.RPoints[j][2], self.RPoints[j+1][2]), c=color)
+                    self.ax.plot3D((self.RPoints[0][0], self.RPoints[0][0]-1),(self.RPoints[0][1], self.RPoints[0][1]),(self.RPoints[0][2], self.RPoints[0][2]), c='yellow')
+                    self.ax.plot3D((self.RPoints[0][0], self.RPoints[0][0]),(self.RPoints[0][1], self.RPoints[0][1]+1),(self.RPoints[0][2], self.RPoints[0][2]), c='green')
+                    self.ax.plot3D((self.RPoints[0][0], self.RPoints[0][0]),(self.RPoints[0][1], self.RPoints[0][1]),(self.RPoints[0][2], self.RPoints[0][2]+1), c='red')
+                    self.tk_plot.draw()
+
+                    #self.Rob.set_freedrive()
+
+
+                else:
+                    self.L1.config(text="FEHLER")
+                    self.L2.config(text="Roboter konnte nicht verbunden werden. Überprüfen und neustarten...", fg='red')
+                    self.Rob.close()
+
+
+        elif (i==1):
+            #if(self.Rob.is_running()):
+            if(True):
+                #pose = self.Rob.getl()
+                #self.Coords.append([pose[0], pose[1], pose[2]])
+                self.L2.config(text="""Bewege den Roboterarm zum zweiten Eckpunkt (siehe Diagramm)
+und drücke Weiter. Auf die Z-Höhe brauchst du nicht zu achten, 
+diese wird der Roboterarm nach dem nächsten "Weiter" automatisch annehmen.""")
+                for artist in self.fig.gca().collections:
+                    if (isinstance(artist.get_gid(), int)):
+                        if (artist.get_gid() >= 0) or (artist.get_gid() <=7):
+                            artist.remove()
+                self.DrawPoints(3,2)
+                self.tk_plot.draw()
+                #self.Rob.set_freedrive()
+                i=i+1
+            else:
+                self.L1.config(text="FEHLER")
+                self.L2.config(text="Roboter hat die Verbindung verloren.. bite prüfen und Neustarten!", fg='red')
+                self.Rob.close()
+        elif (i==2):
+            #if(self.Rob.is_running()):
+            if(True):
+                #pose = self.Rob.getl()
+                #self.Coords.append([pose[0], pose[1], self.Coords[0][2]])
+                #pose[2] = self.Coords[1][2] 
+                #self.Rob.movel(pose)
+                self.L2.config(text="""Bewege den Roboterarm zum dritten Eckpunkt (siehe Diagramm)
+und drücke Weiter. Hier ist nur die Z-Höhe entscheidend, auf X- und Y- brauchst du nicht zu achten. 
+Nachdem du auf Fertig drückst, fährt der Roboterarm alle 8 Eckpunkte ab und der Arbeitsraum wird unter
+Arbeitsraum.csv gespeichert.""")
+                for artist in self.fig.gca().collections:
+                    if (isinstance(artist.get_gid(), int)):
+                        if (artist.get_gid() >= 0) or (artist.get_gid() <=7):
+                            artist.remove()
+                self.DrawPoints(7,3)
+                self.tk_plot.draw()
+                i=i+1
+            else:
+                self.L1.config(text="FEHLER")
+                self.L2.config(text="Roboter hat die Verbindung verloren.. bite prüfen und Neustarten!", fg='red')
+                self.Rob.close()
+        elif (i==3):
+            pass
+        elif (i==1):
+            pass
+
+    def DrawPoints(self,n,k):
+        for j in range(8):
+            if j==n:
+                color='red'
+                self.ax.text(self.Points[j][0]+0.5,self.Points[j][1]-0.5,self.Points[j][2], f'Punkt {k}', 'x', gid=j)
+            else:
+                color='gray'
+            self.ax.scatter3D(self.Points[j][0],self.Points[j][1],self.Points[j][2], c=color, gid=j)
+
+def main():
+    fenster = tk.Tk() #Erzeugen eines Tkinter Objektes
+    instanz = GUI(fenster) #Erzeugen der GUI-Instanz dieses Objektes durch Übergabe an Klasse
+    fenster.mainloop() #Mainloop startet einen Thread mit der GUI => GUI ist somit Main-Thread
+if __name__ == "__main__":
+    main()
+    
--- chesster/Vision-Based-Control/Trainingsdatengenerierung.py
+++ chesster/Vision-Based-Control/Trainingsdatengenerierung.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Author: Thorben Pultke
+Contact: thorben.pultke@gmx.de
+Project: CHESSter
+Summary: Short script for implementing a automated training data generation 
+to work with neural networks as mapping for image COS coords to robot COS coords.
+"""
+
 import cv2 as cv #OpenCV
 import numpy as np
 import urx as urx #UR10e Library for simple Movement
@@ -11,17 +22,18 @@ import os as os
 sys.path.append(os.path.dirname(sys.path[0])) #preperation for import of custom moduls
 from SystemRelatedFunctions.GenericSysFunctions import Printtimer, ImportCSV, ExportCSV #Import of custom moduls
 
+
+
 def main():
     print("This Script is about to produce a specified amount of pictures for training a neural network")
     print("initialisation...")
     Printtimer(1)
 
-    path = os.path.dirname(__file__)
+    IP_ADRESS="XXX.XXX.XXX.XXX"
+    DIRPATH = os.path.dirname(__file__)
 
-    Matrix = ImportCSV(path, "test.csv" , ";")
-    ExportCSV(Matrix, path, "output.csv", ";")
-    print(Matrix)
+    Arbeitsraum = ImportCSV(DIRPATH, "Arbeitsraum.csv" , ";")
+    UR10 = urx.Robot(IP_ADRESS)
 if __name__ == "__main__":
     main()
-    
     

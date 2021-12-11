323cfa54d445bf1bc26321ced10b0319e8c45885
YannikWodarczak <yannik.wodarczak@stud.uni-due.de>
2021-12-11 18:28:53 +0100

    Bauernumwandlung added

--- chesster/Schach_KI/comparison.py
+++ chesster/Schach_KI/comparison.py
@@ -7,12 +7,12 @@ import pandas as pd
 
 #[Zeile1],[Zeile2] etc. aus Robotersicht betrachtet !
 def Matrixvergleich(before, after):
-    Zug=pd.DataFrame(columns=['from','to'])
-    ZugFinal=[]
+    Move=pd.DataFrame(columns=['from','to'])
+    MoveFinal=[]
     alphabet=['a','b','c','d','e','f','g','h']
     #zahlen=[8,7,6,5,4,3,2,1]
-    Zugfrom=pd.DataFrame(columns=['from'])
-    Zugto=pd.DataFrame(columns=['to'])
+    Movefrom=pd.DataFrame(columns=['from'])
+    Moveto=pd.DataFrame(columns=['to'])
     countFrom=0
     countTo=0
     movementdetected=0
@@ -28,9 +28,9 @@ def Matrixvergleich(before, after):
                 iPrint= i + 1
                 jPrint= alphabet[j]
                 print('ausgehende Position ist ' +str(jPrint) + str(iPrint))
-                Zugfrom.loc[countFrom]  = (str(jPrint) + str(iPrint))
+                Movefrom.loc[countFrom]  = (str(jPrint) + str(iPrint))
                 countFrom = countFrom + 1
-                print(Zugfrom)
+                print(Movefrom)
                 movementdetected = movementdetected + 1
                 print('Changes: ' + str(movementdetected+colorchangedetected))
 #Check for MovementToPosition
@@ -38,9 +38,9 @@ def Matrixvergleich(before, after):
                 iPrint = i + 1
                 jPrint = alphabet[j]
                 print('annehmende Position ist ' +str(jPrint) + str(iPrint))
-                Zugto.loc[countTo] = (str(jPrint) + str(iPrint))
+                Moveto.loc[countTo] = (str(jPrint) + str(iPrint))
                 countTo = countTo + 1
-                print(Zugto)
+                print(Moveto)
                 movementdetected = movementdetected + 1
                 print('Changes: ' + str(movementdetected+colorchangedetected))
 #Check for ColorChange
@@ -48,9 +48,9 @@ def Matrixvergleich(before, after):
                 iPrint = i + 1
                 jPrint = alphabet[j]
                 print('annehmende Position ist ' + str(jPrint) + str(iPrint))
-                Zugto.loc[countTo] = (str(jPrint) + str(iPrint))
+                Moveto.loc[countTo] = (str(jPrint) + str(iPrint))
                 countTo = countTo + 1
-                print(Zugto)
+                print(Moveto)
                 colorchangedetected = colorchangedetected + 1
                 print('Changes: ' + str(movementdetected+colorchangedetected))
 # lange weiße Rochade
@@ -58,62 +58,87 @@ def Matrixvergleich(before, after):
             if type(before[0][4]) == str and type(after[0][2]) == str and type(before[0][0]) == str and type(after[0][3]) == str\
                     and before[0][1] == 0 and before[0][2] == 0 and before[0][3] == 0\
                     and after[0][0] == 0 and after[0][1] == 0 and after[0][4] == 0:
-                ZugFinal.extend(['e1c1','a1d1'])
+                MoveFinal.extend(['e1c1','a1d1'])
                 print('Lange weiße Rochade')
 # kurze weiße Rochade
         if i == 0: #kurze weiße Rochade
             if type(before[0][4]) == str and type(after[0][5]) == str\
                     and type(before[0][7]) == str and type(after[0][6]) == str\
                     and before[0][5] == 0 and before[0][6] == 0 and after[0][4] == 0 and after[0][7] == 0:
-                ZugFinal.extend(['e1g1','h1f1'])
+                MoveFinal.extend(['e1g1','h1f1'])
                 print('Kurze weiße Rochade')
 # lange schwarze Rochade
         if i == 7: #lange schwarze Rochade
             if type(before[7][4]) == str and type(after[7][2]) == str and type(before[7][0]) == str and type(after[7][3]) == str and \
                     before[7][1] == 0 and before[7][2] == 0 and before[7][3] == 0 and \
                     after[7][0] == 0 and after[7][1] == 0 and after[7][4] == 0:
-                ZugFinal.extend(['e8c8', 'a8d8'])
+                MoveFinal.extend(['e8c8', 'a8d8'])
                 print('Lange schwarze Rochade')
 # kurze schwarze Rochade
         if i == 7: #kurze schwarze Rochade
             if type(before[7][4]) == str and type(after[7][5]) == str\
                     and type(before[7][7]) == str and type(after[7][6]) == str \
                     and before[7][5] == 0 and before[7][6] == 0 and after[7][4] == 0 and after[7][7] == 0:
-                ZugFinal.extend(['e8g8', 'h8f8'])
+                MoveFinal.extend(['e8g8', 'h8f8'])
                 print('Kurze schwarze Rochade')
 
     if movementdetected + colorchangedetected == 2:
-        Zug.loc[0]=[Zugfrom['from'][0],Zugto['to'][0]]
-        ZugFinal = Zug['from'][0] + Zug['to'][0]
+        Move.loc[0]=[Movefrom['from'][0],Moveto['to'][0]]
+        MoveFinal = Move['from'][0] + Move['to'][0]
     if movementdetected + colorchangedetected <2:
-        print(Zug)
-        ZugFinal = 0
+        print(Move)
+        MoveFinal = 0
     if movementdetected + colorchangedetected > 2:
-        Zug.loc[0] = [Zugfrom['from'][0], Zugto['to'][0]]
-        Zug.loc[1] = [Zugfrom['from'][1],Zugto['to'][1]]
-        #ZugFinal = 'No clear move without extra if-clause'
+        Move.loc[0] = [Movefrom['from'][0], Moveto['to'][0]]
+        Move.loc[1] = [Movefrom['from'][1],Moveto['to'][1]]
+        #MoveFinal = 'No clear move without extra if-clause'
     if colorchangedetected > 0:
         Figurschlag = True
     else:
         Figurschlag = False
-    print(Zug)
+    print(Move)
 
-    return ZugFinal, Figurschlag
+    return MoveFinal, Figurschlag
 
 #[Zeile1],[Zeile2] etc. aus Robotersicht betrachtet !
 def FullMatrixComp(before, after):
-    Zug=pd.DataFrame(columns=['from','to'])
-    ZugFinal=[]
+    Move=pd.DataFrame(columns=['from','to'])
+    MoveFinal=[]
     alphabet=['a','b','c','d','e','f','g','h']
     #zahlen=[8,7,6,5,4,3,2,1]
-    Zugfrom=pd.DataFrame(columns=['from'])
-    Zugto=pd.DataFrame(columns=['to'])
+    Movefrom=pd.DataFrame(columns=['from'])
+    Moveto=pd.DataFrame(columns=['to'])
     countFrom=0
     countTo=0
     movementdetected=0
     colorchangedetected=0 #matrix with distinction in colors
+    countNbefore=0
+    countNafter = 0
+    countnbefore=0
+    countnafter=0
+    countQbefore=0
+    countQafter=0
+    countqbefore = 0
+    countqafter = 0
     for i in range(0,8):
         for j in range(0,8):
+            #Bauernumwandlung
+            if before[i][j] == 'Q':
+                countQbefore=countQbefore + 1
+            if after[i][j] == 'Q':
+                countQafter=countQafter +1
+            if before[i][j] == 'q':
+                countqbefore = countqbefore + 1
+            if after[i][j] == 'q':
+                countqafter = countqafter + 1
+            if before[i][j] == 'N':
+                countNbefore=countNbefore + 1
+            if after[i][j] == 'N':
+                countNafter=countNafter +1
+            if before[i][j] == 'n':
+                countnbefore = countnbefore + 1
+            if after[i][j] == 'n':
+                countnafter = countnafter + 1
             #print(before[i][j])
             #print(after[i][j])
             #if before[i][j] is not after[i][j]:
@@ -123,86 +148,96 @@ def FullMatrixComp(before, after):
                 iPrint= i + 1
                 jPrint= alphabet[j]
                 print(str(before[i][j]) + ' von ' + str(jPrint) + str(iPrint))
-                Zugfrom.loc[countFrom]  = (str(jPrint) + str(iPrint))
+                Movefrom.loc[countFrom]  = (str(jPrint) + str(iPrint))
                 countFrom = countFrom + 1
-                #print(Zugfrom)
+                print(Movefrom)
                 movementdetected = movementdetected + 1
-                #print('Changes: ' + str(movementdetected+colorchangedetected))
+                print('Changes: ' + str(movementdetected+colorchangedetected))
 #Check for MovementToPosition
             if before[i][j] == 0 and (type(after[i][j]) ==str):
                 iPrint = i + 1
                 jPrint = alphabet[j]
                 print(str(after[i][j]) + ' nach ' + str(jPrint) + str(iPrint))
-                Zugto.loc[countTo] = (str(jPrint) + str(iPrint))
+                Moveto.loc[countTo] = (str(jPrint) + str(iPrint))
                 countTo = countTo + 1
-                #print(Zugto)
+                print(Moveto)
                 movementdetected = movementdetected + 1
-                #print('Changes: ' + str(movementdetected+colorchangedetected))
+                print('Changes: ' + str(movementdetected+colorchangedetected))
 #Check for ColorChange
             if not type(after[i][j])==int and not type(before[i][j])==int:
                 if (str.isupper(after[i][j]) and str.islower(before[i][j])) or (str.islower(after[i][j]) and str.isupper(before[i][j])):
                     iPrint = i + 1
                     jPrint = alphabet[j]
                     print(str(after[i][j]) + ' nach ' + str(jPrint) + str(iPrint))
-                    Zugto.loc[countTo] = (str(jPrint) + str(iPrint))
+                    Moveto.loc[countTo] = (str(jPrint) + str(iPrint))
                     countTo = countTo + 1
-                    #print(Zugto)
+                    print(Moveto)
                     colorchangedetected = colorchangedetected + 1
-                    #print('Changes: ' + str(movementdetected+colorchangedetected))
+                    print('Changes: ' + str(movementdetected+colorchangedetected))
 # lange weiße Rochade
         if i == 0: #lange weiße Rochade
             if before[0][4] == 'K' and after[0][2] == 'K' and before[0][0] == 'R'and after[0][3] == 'R':#\
                     #and before[0][1] == 0 and before[0][2] == 0 and before[0][3] == 0\
                     #and after[0][0] == 0 and after[0][1] == 0 and after[0][4] == 0:
-                ZugFinal.extend(['e1c1','a1d1'])
+                MoveFinal.extend(['e1c1','a1d1'])
                 print('Lange weiße Rochade')
 # kurze weiße Rochade
         if i == 0: #kurze weiße Rochade
             if before[0][4] == 'K' and after[0][5] == 'R'\
                     and before[0][7] == 'R' and after[0][6] == 'K':#\
                     #and before[0][5] == 0 and before[0][6] == 0 and after[0][4] == 0 and after[0][7] == 0:
-                ZugFinal.extend(['e1g1','h1f1'])
+                MoveFinal.extend(['e1g1','h1f1'])
                 print('Kurze weiße Rochade')
 # lange schwarze Rochade
         if i == 7: #lange schwarze Rochade
             if before[7][4] == 'k' and after[7][2] == 'k' and before[7][0] == 'r' and after[7][3] == 'r':# and #\
                     #before[7][1] == 0 and before[7][2] == 0 and before[7][3] == 0 and \
                     #after[7][0] == 0 and after[7][1] == 0 and after[7][4] == 0:
-                ZugFinal.extend(['e8c8', 'a8d8'])
+                MoveFinal.extend(['e8c8', 'a8d8'])
                 print('Lange schwarze Rochade')
 # kurze schwarze Rochade
         if i == 7: #kurze schwarze Rochade
             if before[7][4] == 'k' and after[7][5] == 'r'\
                     and before[7][7] == 'r' and after[7][6] == 'k':#\
                     #and before[7][5] == 0 and before[7][6] == 0 and after[7][4] == 0 and after[7][7] == 0:
-                ZugFinal.extend(['e8g8', 'h8f8'])
+                MoveFinal.extend(['e8g8', 'h8f8'])
                 print('Kurze schwarze Rochade')
-# Bauernumwandlung
-
 
 
     if movementdetected + colorchangedetected == 2:
-        Zug.loc[0]=[Zugfrom['from'][0],Zugto['to'][0]]
-        ZugFinal = Zug['from'][0] + Zug['to'][0]
+        Move.loc[0]=[Movefrom['from'][0],Moveto['to'][0]]
+        MoveFinal = Move['from'][0] + Move['to'][0]
+        if (countQafter > countQbefore):
+            Move.loc[0] = [Movefrom['from'][0], Moveto['to'][0]]
+            MoveFinal = Move['from'][0] + Move['to'][0] + 'Q'
+        if (countqafter > countqbefore):
+            Move.loc[0] = [Movefrom['from'][0], Moveto['to'][0]]
+            MoveFinal = Move['from'][0] + Move['to'][0] + 'q'
+        if (countNafter > countNbefore):
+            Move.loc[0] = [Movefrom['from'][0], Moveto['to'][0]]
+            MoveFinal = Move['from'][0] + Move['to'][0] + 'N'
+        if (countnafter > countnbefore):
+            Move.loc[0] = [Movefrom['from'][0], Moveto['to'][0]]
+            MoveFinal = Move['from'][0] + Move['to'][0] + 'n'
     if movementdetected + colorchangedetected <2:
-        #print(Zug)
-        ZugFinal = 0
+        #print(Move)
+        MoveFinal = 0
     if movementdetected + colorchangedetected > 2:
-        Zug.loc[0] = [Zugfrom['from'][0], Zugto['to'][0]]
-        Zug.loc[1] = [Zugfrom['from'][1],Zugto['to'][1]]
-        #ZugFinal = 'No clear move without extra if-clause'
+        Move.loc[0] = [Movefrom['from'][0],Moveto['to'][0]]
+        Move.loc[1] = [Movefrom['from'][1],Moveto['to'][1]]
+        #MoveFinal = 'No clear move without extra if-clause'
     if colorchangedetected > 0:
         Figurschlag = True
     else:
         Figurschlag = False
-    #print(Zug)
-    print(ZugFinal)
+    #print(Move)
+    print(MoveFinal)
 
-    return ZugFinal, Figurschlag
+    return MoveFinal, Figurschlag
 
 #Figurschlag: *Schlag
 #Rochaden: lang:L, kurz: K, weiß: W, schwarz:S
 #stockfish.set_fen_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
 #bestmove=stockfish.get_best_move()
-#stockfish.make_moves_from_current_position([ermittelterZug])
+#stockfish.make_moves_from_current_position([ermittelterMove])
 #print(stockfish.get_board_visual())

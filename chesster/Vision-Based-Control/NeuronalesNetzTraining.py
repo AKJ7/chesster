36d6a9758a708ae6e899a0542c261b42297372e7
Thorben Pultke <thorben.pultke@gmx.de>
2021-12-12 13:32:11 +0100

    Changes to Data-read-in

--- chesster/Vision-Based-Control/NeuronalesNetzTraining.py
+++ chesster/Vision-Based-Control/NeuronalesNetzTraining.py
@@ -1,9 +1,13 @@
 import numpy as np
-from keras.models import Sequential, sequential
+from keras.models import Sequential
 from keras.layers import Dense
-from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
+from tensorflow.keras.callbacks import TensorBoard
 from sklearn.datasets import make_regression
 import os as os
+import sys as sys
+sys.path.append(os.path.dirname(sys.path[0]))
+from SystemRelatedFunctions.GenericSysFunctions import ImportCSV
+
 def get_Model(n_input, n_output, n_Dense, n_nodes):
     model = Sequential()
     model.add(Dense(n_nodes, input_dim=n_input, kernel_initializer='he_uniform', activation='relu')) #INPUT-LAYER
@@ -17,18 +21,20 @@ def get_Model(n_input, n_output, n_Dense, n_nodes):
     NAME = f'nD{n_Dense}_nN{n_nodes}'
     return model, NAME
 
-def get_Data():
+def get_Data_test():
     X, Y = make_regression(n_samples=200, n_features=3, n_informative=3, n_targets=3, random_state=2)
     return X, Y
 
-""""
-def get_Data(Input, Output, Path):
-    pass
+def get_Data():
+    DIRPATH = os.path.dirname(__file__)
+    Dir = DIRPATH+"/Trainingsdaten/"
+    X = ImportCSV(Dir, "Input.csv", ";")
+    Y = ImportCSV(Dir, "Output.csv", ";")
     return X, Y
-"""
+
 
 def train():
-    X, Y = get_Data()
+    X, Y = get_Data_test()
     n_data = X.shape[0]
     Epochs = 200
     model, NAME = get_Model(3, 3, 3, 40)

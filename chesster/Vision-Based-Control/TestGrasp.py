74d6505692075ddd4882daac3a11fcab02331a61
Thorben Pultke <thorben.pultke@gmx.de>
2021-12-27 16:54:51 +0100

    Small changes for grasping with VBC

--- chesster/Vision-Based-Control/TestGrasp.py
+++ chesster/Vision-Based-Control/TestGrasp.py
@@ -20,19 +20,24 @@ d_img=[]
 def onmouse(event, x, y, flags, param):
     global c_img, d_img, model, Robot
     if event == cv.EVENT_LBUTTONDOWN:
+        x_old_ptp = 1035.0
+        x_old_min = 40.0
+        y_old_ptp = 1120.9
+        y_old_min = -897.0
         pos = (x,y)
         Input = np.array([x, y, d_img[x,y]])
         Input = Input[np.newaxis, :]
+        Input = 2.*(Input - x_old_min)/x_old_ptp-1 #normalization
         print(Input)
         print(Input.shape)
         cv.circle(c_img, (x,y), 3, [0, 0, 255], -1)
         cv.putText(c_img, f"IMG CORDS: x:{x} - y:{y} - depth:{d_img[x,y]}", (x-40, y-20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-        
-        
-        Input = np.array([1.280000000000000000e+02, 3.180000000000000000e+02, 1.019000000000000000e+03])
-        Input = Input[np.newaxis, :]
+
+        #Input = np.array([1.280000000000000000e+02, 3.180000000000000000e+02, 1.019000000000000000e+03])
+        #Input = Input[np.newaxis, :]
         prediction = model.predict(Input)
         prediction = np.round(prediction, decimals=2)
+        prediction = ((prediction+1)*y_old_ptp)/2+y_old_min
         print(prediction)
 
         cv.putText(c_img, f"PREDICTION: x:{prediction[0, 0]} - y:{prediction[0, 1]} - z:{prediction[0, 2]}", (x-40, y-60), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
@@ -40,7 +45,7 @@ def onmouse(event, x, y, flags, param):
 
         ORIENTATION = np.array([0,-3.143, 0]) #Standard Orientation for Grasping
         #ORIENTATION = np.array([0, 2.220, -2.220]) #Standard Orientation for Grasping
-        Pose_offset = np.array([prediction[0, 0], prediction[0, 1], prediction[0, 2]+20, ORIENTATION[0], ORIENTATION[1], ORIENTATION[2]])
+        Pose_offset = np.array([prediction[0, 0], prediction[0, 1], 100, ORIENTATION[0], ORIENTATION[1], ORIENTATION[2]])
 
         Robot.MoveC(Pose_offset)
         print(Robot.WhereC())
@@ -54,7 +59,7 @@ def main():
     Robot.MoveJ(TRAINING_HOME)
     
     NAME = "nD5_nN40_nData200_nEpochs200"
-    NAME = "Test"
+    NAME = "TEST_FLAT"
     model = keras.models.load_model("C:/NN/"+NAME)
     Camera = RealSenseCamera()  
     while True:

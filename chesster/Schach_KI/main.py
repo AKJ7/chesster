cc7298b7f929893438257d87a9b8954b587f572e
YannikWodarczak <yannik.wodarczak@stud.uni-due.de>
2021-12-29 09:53:14 +0100

    Updates, gameplay testet with comparison generated by fen position

--- chesster/Schach_KI/main.py
+++ chesster/Schach_KI/main.py
@@ -1,3 +1,60 @@
+from chesster.Schach_KI.gameplay import *
+from chesster.Schach_KI.comparison import *
+from stockfish import Stockfish
+#stockfish = Stockfish("C:\\Users\\ywoda\\PycharmProjects\\Chesster\\chesster\\stockfish_14.1_win_x64_avx2.exe",
+                      #parameters={"Threads": 4, "Minimum Thinking Time":30, "Skill Level": 50})
+before=[]
+#stockfish.set_fen_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+#print(stockfish.get_board_visual())
+# MatrixBefore=Funktion Objekterkennung
+player_color='w'
+VBCcomm = start_game(player_color)
+if VBCcomm != []:
+    print(VBCcomm)
+#while Checkmate == False:
+for i in range(200):
+    # Spielerzug → warten auf Bestätigung
+    # MatrixAfter=Funktion Objekterkennung
+    # If Fehler in Matrix (Funktion fehlt)
+        #Move=Matrixvergleich(MatrixBefore,MatrixAfter)
+    # else:
+        #Move=FullMatrixcomp(MatrixBefore,MatrixAfter)
+    Move=eval(input('Spielerzug?'))
+    Figurschlag=input('Figur geschlagen?')
+    VBCcommand,zug_zulässig, SchachSpieler, SchachKI, SchachmattKI, SchachmattSpieler = gameplay(Move, Figurschlag, before, player_color)
+    if zug_zulässig == True:
+        print('KI is performing the following move(s): ' + str(VBCcommand))#,zug_zulässig)
+    else:
+        print('Rollbackmove: ' + str(VBCcommand))
+    if SchachSpieler==True:
+        print('Spieler steht im Schach')
+    if SchachKI==True:
+        print('KI steht im Schach')
+    if SchachmattSpieler==True:
+        print('Spieler steht im Schachmatt')
+        break
+    if SchachmattKI== True:
+        print('KI steht im Schachmatt')
+        break
+
+print('Game has come to an end')
+    # GUI-Ausgabe (Schach
+    #Funktion VBC(VBCcommand)
+    #If Zug zulässig==True
+        #MatrixBefore=MatrixAfter
+        #MatrixAfter=Funktion Objekterkennung
+        # If Fehler in Matrix (Funktion fehlt)
+        # MoveKI=Matrixvergleich(MatrixBefore,MatrixAfter)
+        # else:
+        # MoveKI=FullMatrixcomp(MatrixBefore,MatrixAfter)
+        # MatrixBefore=MatrixAfter
+
+
+        #(((
+        # if MoveKI <> VBCcommand:
+            #Funktion Rollback(MoveKI)
+            #Funktion VBC   )))
+
 
 
 #Funktion Spielstart - Press "Start Game" on GUI
@@ -13,10 +70,9 @@
 #Funktion Schach_KI
 ##Output: Zugzulässigkeit, Roboterzug
 #Funktion: VBC
-if Zugzulässigkeit = true
-    MatrixBefore=MatrixAfter
+#if Zugzulässigkeit = true
+    #MatrixBefore=MatrixAfter
     #Funktion Objekterkennung
     ##Output: MatrixAfter
-.....
 
 
--- chesster/Schach_KI/tests.py
+++ chesster/Schach_KI/tests.py
@@ -2,6 +2,7 @@ import chess
 import chess.engine
 import pandas as pd
 from chesster.Schach_KI.comparison import *
+from chesster.Schach_KI.gameplay import *
 
 #[Zeile1],[Zeile2] etc. aus Robotersicht betrachtet !
 beforeSchlag=[['W','W','W','W','W','W','W','W'],
@@ -90,21 +91,21 @@ afterKS=[['W','W','W','W','W','W','W','W'],
        ['B','B','B','B',0,'B','B',0]]
 ############################################With detail informaton########
 #[Zeile1],[Zeile2] etc. aus Robotersicht betrachtet !
-beforeHit=[['R','N','B','Q','K','B','N','R'],
-       ['P','P','P','P','P','P','P','P'],
+beforeHit=[['R','N','B','Q','K','B',0,'R'],
+           ['P','P','P',0,'P','P','P','P'],
+           [0,0,0,0,0,'N',0,0],
+           [0,0,0,'P','p',0,0,0],
+           [0,0,0,0,0,0,0,0],
+           [0,0,0,0,0,0,0,0],
+           ['p','p','p','p',0,'p','p','p'],
+           ['r','n','b','q','k','b','n','r']]
+afterHit=[['R','N','B','Q','K','B',0,'R'],
+       ['P','P','P',0,'P','P','P','P'],
+       [0,0,0,'p',0,'N',0,0],
        [0,0,0,0,0,0,0,0],
        [0,0,0,0,0,0,0,0],
        [0,0,0,0,0,0,0,0],
-       ['p',0,0,0,0,0,0,0],
-       [0,'p','p','p','p','p','p','p'],
-       ['r','n','b','q','k','b','n','r']]
-afterHit=[['R','N','B','Q','K','B','N','R'],
-       ['P','P','P','P',0,'P','P','P'],
-       [0,0,0,0,0,0,0,0],
-       [0,0,0,0,0,0,0,0],
-       [0,0,0,0,0,0,0,0],
-       ['P',0,0,0,0,0,0,0],
-       [0,'p','p','p','p','p','p','p'],
+       ['p','p','p','p',0,'p','p','p'],
        ['r','n','b','q','k','b','n','r']]
 #lange weiße Rochade
 beforeLW=[['R',0,0,0,'K','B','N','R'],
@@ -175,8 +176,69 @@ afterKS=[['R','N','B','Q','K','B','N','R'],
        ['p','p','p','p','p','p','p','p'],
        ['r','n','b','q',0,'r','k',0]]
 
-FullMatrixComp(beforeKS,afterKS)
-FullMatrixComp(beforeKW,afterKW)
-FullMatrixComp(beforeLS,afterLS)
-FullMatrixComp(beforeLW,afterLW)
-FullMatrixComp(beforeHit,afterHit)
+#FullMatrixComp(beforeKS,afterKS,player_color)
+#FullMatrixComp(beforeKW,afterKW)
+#FullMatrixComp(beforeLS,afterLS)
+#FullMatrixComp(beforeLW,afterLW)
+player_color='b'
+move,Hitting=FullMatrixComp(beforeKS,afterKS,player_color)
+print(move)
+move,Hit=FullMatrixComp(beforeHit,afterHit,player_color)
+print(move)
+#beforeHit=[['R','N','B','Q','K','B','N','R'],
+       #['P','P','P','P','P','P','P','P'],
+       #[0,0,0,0,0,0,0,0],
+       #[0,0,0,0,0,0,0,0],
+       #[0,0,0,0,0,0,0,0],
+       #[0,0,0,0,0,0,0,0],
+       #['p','p','p','p','p','p','p','p'],
+       #['r','n','b','q','k','b','n','r']]
+stockfish.set_fen_position("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+print(stockfish.get_board_visual())
+stockfish.make_moves_from_current_position(['f2f4','g8h6','f4f5'])
+stockfish.make_moves_from_current_position(['f2f4','g8h6','f4f5'])
+print(stockfish.get_board_visual())
+print(stockfish.get_fen_position())
+#stockfish.make_moves_from_current_position(['h7h5','f2f4'])
+#print(stockfish.get_board_visual())
+#print(stockfish.get_fen_position())
+move=["e7e5"]
+cmd=gameplay(move,False,afterHit,player_color)
+print(cmd)
+
+print(stockfish.get_evaluation())
+computebefore = str(stockfish.get_fen_position())
+computebefore = computebefore.replace(str(8), "00000000")
+computebefore = computebefore.replace(str(7), "0000000")
+computebefore = computebefore.replace(str(6), "000000")
+computebefore = computebefore.replace(str(5), "00000")
+computebefore = computebefore.replace(str(4), "0000")
+computebefore = computebefore.replace(str(3), "000")
+computebefore = computebefore.replace(str(2), "00")
+computebefore = computebefore.replace(str(1), "0")
+computebefore = computebefore.replace("/", "")
+computebefore64 = computebefore[0:64]
+list = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+list2 = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
+for i in range(len(computebefore64)):
+ maindiv = int((i) / 8)
+ rest = int((i) % 8)
+ # print(type(computebefore64[i]))
+ if computebefore64[i] == '0':
+     list[maindiv][rest] = 0
+ else:
+     list[maindiv][rest] = computebefore64[i]
+count=7
+for n in range(0,8):
+ list2[n]=list[count]
+ count = count-1
+print(list2)
+
+
+
+
+#[hallo],string=start_game('w')
+#print([hallo])
+#print(string)

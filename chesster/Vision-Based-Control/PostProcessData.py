f6b441c278e913a17008ee3657e98c5d6ea4a7f8
Thorben Pultke <thorben.pultke@gmx.de>
2022-01-07 14:44:26 +0100

    Renamed plot data to PostProcessData and added minor improvments

--- chesster/Vision-Based-Control/PostProcessData.py
+++ chesster/Vision-Based-Control/PostProcessData.py
@@ -7,27 +7,29 @@ from moduls.GenericSysFunctions import ImportCSV, ExportCSV
 import numpy as np
 import numpy as np
 import statsmodels.api as sm
+
 def reg_m(y, x):
     x = np.array(x).T
     x = sm.add_constant(x)
     results = sm.OLS(endog=y, exog=x).fit()
     return results
+
 def lin_reg_result(X, Y, coeff):
     Z = coeff[1]*X+coeff[2]*Y+coeff[0]
     return Z
+
 DIRPATH = os.path.dirname(__file__)
-DIRPATH = "C:/Users/thorb/iCloudDrive/Studium/UDE/Semester 2/Mechatroniklabor/Schachroboter/Quellcode/chesster-aktuell/chesster/Vision-Based-Control"
+#DIRPATH = "C:/Users/admin/Desktop/ML/chesster/chesster/Vision-Based-Control" #Zuhause
+DIRPATH = "C:/Users/admin/Desktop/ML/chesster/chesster/Vision-Based-Control" #Uni
 Dir = DIRPATH+"/Trainingsdaten/"
-X = ImportCSV(Dir, "Input1000.csv", ";")
-Y = ImportCSV(Dir, "Output1000.csv", ";")
-
-
-
+X = ImportCSV(Dir, "Input3000.csv", ";")
+Y = ImportCSV(Dir, "Output3000.csv", ";")
+filterLin = False
 fig = plt.figure()
 ax = fig.add_subplot(211, projection='3d')
 
 ax2 = fig.add_subplot(221, projection='3d')
-ax.set_title('Simple Filtering')
+ax.set_title(f'Raw Data n={X.shape[1]}')
 ax.set_xlabel('x [px]')
 ax.set_ylabel('y [px]')
 ax.set_zlabel('Depth [mm]')
@@ -36,13 +38,16 @@ ax2.set_ylabel('Y Robot KOS [mm]')
 ax2.set_zlabel('Z Robot KOS [mm]')
 ax3 = fig.add_subplot(212, projection='3d')
 ax4 = fig.add_subplot(222, projection='3d')
-ax3.set_title('Advanced Filtering')
+
 ax3.set_xlabel('x [px]')
 ax3.set_ylabel('y [px]')
 ax3.set_zlabel('Depth [mm]')
 ax4.set_xlabel('X Robot KOS [mm]')
 ax4.set_ylabel('Y Robot KOS [mm]')
 ax4.set_zlabel('Z Robot KOS [mm]')
+
+ax.scatter(X[0,:], X[1,:], X[2,:], c='red', marker='o')
+ax2.scatter(Y[0,:], Y[1,:], Y[2,:], c='red', marker='o')
 
 print(X.shape[1])
 
@@ -55,39 +60,40 @@ X_Hat = X.copy()
 X = X[:, X_Hat[2,:]<1100]
 Y = Y[:, X_Hat[2,:]<1100]
 X_Hat = X.copy()
-"""
-ax.scatter(X[0,:], X[1,:], X[2,:], c='red', marker='o')
-ax2.scatter(Y[0,:], Y[1,:], Y[2,:], c='red', marker='o')
-result = reg_m(X[2,:], X[0:2,:])
-print(result.summary())
 
-coeff = result.params
-x_reg = np.linspace(np.min(X[0,:]), np.max(X[0,:]), num=500)
-y_reg = np.linspace(np.min(X[1,:]),np.max(X[1,:]), num=500)
-meshx, meshy = np.meshgrid(x_reg, y_reg)
-Z_reg = coeff[1]*meshx+coeff[2]*meshy+coeff[0]
-z_scatter = coeff[1]*X[0,0]+coeff[2]*X[1,0]+coeff[0]
-surf = ax.plot_surface(meshx, meshy, Z_reg,
-                       linewidth=0, antialiased=False)
-ax.scatter(X[0,0], X[1,0], z_scatter, c='green', marker="*")
-print(X.shape[1])
+X = X[:, X_Hat[0,:]!=0]
+Y = Y[:, X_Hat[0,:]!=0]
 
-sigma = 40
+if filterLin == True:
+    result = reg_m(X[2,:], X[0:2,:])
+    print(result.summary())
 
-X = X[:, X_Hat[2,:]>lin_reg_result(X_Hat[0,:], X_Hat[1,:], coeff)-sigma]
-Y = Y[:, X_Hat[2,:]>lin_reg_result(X_Hat[0,:], X_Hat[1,:], coeff)-sigma]
-X_Hat = X.copy()
-X = X[:, X_Hat[2,:]<lin_reg_result(X_Hat[0,:], X_Hat[1,:], coeff)+sigma]
-Y = Y[:, X_Hat[2,:]<lin_reg_result(X_Hat[0,:], X_Hat[1,:], coeff)+sigma]
-print(X.shape[1])
+    coeff = result.params
+    print(coeff)
+    x_reg = np.linspace(np.min(X[0,:]), np.max(X[0,:]), num=500)
+    y_reg = np.linspace(np.min(X[1,:]),np.max(X[1,:]), num=500)
+    meshx, meshy = np.meshgrid(x_reg, y_reg)
+    Z_reg = coeff[1]*meshx+coeff[2]*meshy+coeff[0]
+    z_scatter = coeff[1]*X[0,0]+coeff[2]*X[1,0]+coeff[0]
+    surf = ax.plot_surface(meshx, meshy, Z_reg,
+                        linewidth=0, antialiased=False)
+    ax.scatter(X[0,0], X[1,0], z_scatter, c='green', marker="*")
+    print(X.shape[1])
 
-X[2,:] = lin_reg_result(X[0,:], X[1,:], coeff)
-"""
+    sigma = 40
+
+    X = X[:, X_Hat[2,:]>lin_reg_result(X_Hat[0,:], X_Hat[1,:], coeff)-sigma]
+    Y = Y[:, X_Hat[2,:]>lin_reg_result(X_Hat[0,:], X_Hat[1,:], coeff)-sigma]
+    X_Hat = X.copy()
+    X = X[:, X_Hat[2,:]<lin_reg_result(X_Hat[0,:], X_Hat[1,:], coeff)+sigma]
+    Y = Y[:, X_Hat[2,:]<lin_reg_result(X_Hat[0,:], X_Hat[1,:], coeff)+sigma]
+    print(X.shape[1])
+
 ax3.scatter(X[0,:], X[1,:], X[2,:], c='red', marker='o')
 ax4.scatter(Y[0,:], Y[1,:], Y[2,:], c='red', marker='o')
 DIRPATH = os.path.dirname(__file__)
 DirOutput = DIRPATH+"/Trainingsdaten/"
 ExportCSV(X, DirOutput, f'Input{X.shape[1]}Filtered.csv', ';')
 ExportCSV(Y, DirOutput, f'Output{X.shape[1]}Filtered.csv', ';')
-
+ax3.set_title(f'Filtered Data n={X.shape[1]}')
 plt.show()

23f9e8a967984292ac6dde492fbf830a293e6690
Thorben Pultke <thorben.pultke@gmx.de>
2022-01-30 19:31:02 +0100

    Changes to integration test 2

--- tests/integration/Integration_02_OBJ_R+VBC+CHESS_AI.py
+++ tests/integration/Integration_02_OBJ_R+VBC+CHESS_AI.py
@@ -1,5 +1,5 @@
 from chesster.vision_based_control.controller import VisualBasedController
-from chesster.obj_recognition.chessboard_recognition import ChessBoard
+from chesster.obj_recognition import *
 from chesster.camera.realsense import RealSenseCamera
 from chesster.Robot.UR10 import UR10Robot
 from chesster.Schach_KI.class_chess_gameplay import ChessGameplay
@@ -8,7 +8,9 @@ if __name__ == "__main__":
     robot = UR10Robot()
     camera = RealSenseCamera()
     vbc = VisualBasedController(robot, "/")
-    chessboard = ChessBoard()
+    detector = ObjectRecognition()
+    detector.start()
+    chessPieces_Old = detector.get_chesspiece_info() 
     ChessAI = ChessGameplay(skill_level=5, threads=4, minimum_thinking_time=30, debug=False)
     i=0
     CheckMate=False
@@ -19,25 +21,33 @@ if __name__ == "__main__":
     robotColor = input('please select the color of the robot/AI (b/w): ')
 
     if robotColor == "w":
-        i=i+1
         humanColor = "b"
     else:
         humanColor = "w"
-
+    
     while CheckMate == False:
-        temp = input('press enter when your move is completed...')
-        print('###################')
-        print(f'Robot Action {i}')
+        if i==0 and robotColor=="w":
+            print('###################')
+            print(f'Robot Action {i}')
+            print('Robot starts the game.')
+            print('CHESS_AI: Calculating best move...')
+            actions, _, CheckMate, _ = ChessAI.play_ki(ChessPieces_Old, humanColor)
+            print('CHESS_AI: Best move calculated!')
+        else:
+            temp = input('press enter when your move is completed...')
+            print('###################')
+            print(f'Robot Action {i}')
 
-        print('OBJ_R: Scanning Chessboard and calculating move done by player...')
-        ChessPieces_New = chessboard.Scan() #Platzhalter
-        print('OBJ_R: Scanning done.')
+            print('OBJ_R: Scanning Chessboard and calculating move done by player...')
+            detector.determine_changes()
+            chessPieces_New = detector.get_chesspiece_info()
+            print('OBJ_R: Scanning done.')
 
-        print('CHESS_AI: Calculating best move...')
-        moveHuman, _ = ChessAI.piece_notation_comparison(ChessPieces_Old, ChessPieces_New, humanColor)
-        ChessAI.play_opponent(moveHuman)
-        actions, _, CheckMate, _ = ChessAI.play_ki(ChessPieces_Old, humanColor)
-        print('CHESS_AI: Best move calculated!')
+            print('CHESS_AI: Calculating best move...')
+            moveHuman, _ = ChessAI.piece_notation_comparison(chessPieces_Old, chessPieces_New, humanColor)
+            ChessAI.play_opponent(moveHuman) --> TBD
+            actions, _, CheckMate, _ = ChessAI.play_ki(ChessPieces_Old, humanColor)
+            print('CHESS_AI: Best move calculated!')
 
         print('VBC: Proceeding to perform move by AI...')
         for move in actions:
@@ -45,8 +55,10 @@ if __name__ == "__main__":
             vbc.useVBC(move, ChessPieces)
         print('VBC: Move done!')
         print('')
+        detector.determine_changes()
+        ChessPieces_Old = detector.get_chesspiece_info() #Platzhalter
         print('Its your turn again!')
-        ChessPieces_Old = chessboard.Scan() #Platzhalter
+        i=i+1
         
     print('GAME OVER!')
         

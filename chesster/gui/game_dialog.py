b9c55a9cc441cac43c9580be7e8ad19335edb331
Thorben Pultke <thorben.pultke@gmx.de>
2022-02-06 18:50:40 +0100

    Added main function call from hypervisor for game procedure

--- chesster/gui/game_dialog.py
+++ chesster/gui/game_dialog.py
@@ -2,12 +2,14 @@ from PyQt5 import QtGui, QtSvg, QtWidgets, QtCore
 from PyQt5.QtWidgets import QDialog, QLabel, QMessageBox
 from PyQt5.QtGui import QColor, QFont, QImage, QPainter, QPainterPath, QPixmap
 from PyQt5.QtSvg import QSvgWidget, QGraphicsSvgItem
+from sklearn.ensemble import VotingClassifier
 #from chesster import Schach_KI
 #from chesster.Schach_KI.main import VBC_command
 from chesster.gui.utils import get_ui_resource_path
 from PyQt5.uic import loadUi
 from chesster.chess_engine.chess_engine import ChessEngine
 from chesster.Schach_KI.class_chess_gameplay import ChessGameplay
+from chesster.master.hypervisor import Hypervisor
 import logging
 logger = logging.getLogger(__name__)
 
@@ -15,32 +17,59 @@ logger = logging.getLogger(__name__)
 class GameDialog(QDialog):
     def __init__(self, chess_engine_difficulty, player_color, parent=None):
         super(GameDialog, self).__init__(parent)
+        logger.info('Starting Game')
         ui_path = get_ui_resource_path('game_dialog.ui')
         loadUi(ui_path, self)
+        self.Checkmate=False
+        self.game_state="NoCheckmate"
         self.__player_color = player_color
+        logger.info('Setting player colors')
+        if self.__player_color == 'w':
+            logger.info('Human: White | Robot: Black')
+            self.__robot_color = 'b'
+        else:
+            logger.info('Human: Black | Robot: White')
+            self.__robot_color = 'w'
         self.svg_widget = QSvgWidget()
         self.svg_widget.sizeHint()
         self.svg_widget.setMinimumSize(512, 512)
         self.svg_widget.adjustSize()
         self.gridLayout.addWidget(self.svg_widget)
-        self.engine = ChessGameplay(chess_engine_difficulty)
-        image = self.engine.get_drawing()
+        logger.info('initializing hypervisor')
+        self.hypervisor = Hypervisor("/",self.notify, self.__robot_color, self.__player_color) #TBD
+        logger.info('Hypervisior initialized')
+        logger.info('Starting hypervisor')
+        self.hypervisor.start()
+        logger.info('Hypervisor started')
+        image = self.hypervisor.chess_engine.get_drawing()
         self.update_drawing(image)
         logger.info(f'Game Dialog Box started with difficulty: {chess_engine_difficulty} and player color: {player_color}')
-        #self.engine.play()
-        image = self.engine.get_drawing()
-        self.update_drawing(image)
         self.actionAccept.triggered.connect(self.turn_completed)
+
+        if self.__robot_color == 'w':
+            self.hypervisor.robot.StartGesture(Beginner=True)
+            self.game_state = self.hypervisor.make_move(start=True)
+            image = self.hypervisor.chess_engine.get_drawing()
+            self.update_drawing(image)
+        else:
+            self.hypervisor.robot.StartGesture(Beginner=False)
     
     def turn_completed(self) -> None:
+        """
+        main procedure for the game
+        """
         logger.info('Player\'s turn confirmed')
-        Move, CheckMate_KI, CheckMate_Player, _ = self.engine.play_ki([], self.__player_color)
-        image = self.engine.get_drawing()
+        self.game_state = self.hypervisor.make_move(start=False)
+        image = self.hypervisor.make_move(start=False)
         self.update_drawing(image)
+
+        if self.game_state != "NoCheckmate":
+            self.Checkmate = True
+            self.end_game(self.game_state)
 
     def closeEvent(self, a0: QtGui.QCloseEvent) -> None:
         logger.info('Closing')
-        self.engine.stop()
+        self.hypervisor.stop()
         super(GameDialog, self).closeEvent(a0)
 
     def update_drawing(self, svg_image):
@@ -53,3 +82,14 @@ class GameDialog(QDialog):
         message_box.setWindowTitle(title)
         message_box.setText(message)
         message_box.exec()
+
+    def end_game(self, state):
+        """
+        End of the game procedure with Endgestures and space for messages
+        """
+        if state=="RobotVictory":
+            self.hypervisor.robot.EndGesture(Victory=True)
+        else:
+            self.hypervisor.robot.EndGesture(Victory=False)
+
+        self.closeEvent()

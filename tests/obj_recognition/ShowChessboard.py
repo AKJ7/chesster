ce94c0a8ef0dc2c9bb4e59d5e1ebaa37ae57bcfc
Thorben Pultke <thorben.pultke@gmx.de>
2022-02-24 21:55:16 +0100

    Tests for Calibrate data

--- tests/obj_recognition/ShowChessboard.py
+++ tests/obj_recognition/ShowChessboard.py
@@ -1,14 +1,22 @@
 from chesster.obj_recognition.object_recognition import ObjectRecognition
 import cv2 as cv
 import os
-
+import numpy as np
 path = "C:/Mechatroniklabor/chesster/chesster/resources/CalibrationData/chessboard_data.pkl"
 detector = ObjectRecognition(path)
 
+"""
 img_previous = cv.imread("C:/Mechatroniklabor/chesster/previous.png")
 img_current = cv.imread("C:/Mechatroniklabor/chesster/current.png")
 
 detector.board.draw(img_current)
 
 cv.imshow("Current IMG with ROI", img_current)
+cv.waitKey(0)
+"""
+CImg = detector.board.image.copy()
+resized = cv.resize(CImg, (848, 480), interpolation = cv.INTER_CUBIC)
+detector.board.draw(resized)
+cv.imshow("test",resized)
+cv.imwrite('TestBild.png',resized)
 cv.waitKey(0)

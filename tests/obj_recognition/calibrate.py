3edf9b09e3de256202087dcb3c026df9814b3996
Thorben Pultke <thorben.pultke@gmx.de>
2022-02-24 21:54:51 +0100

    no changes

--- tests/obj_recognition/calibrate.py
+++ tests/obj_recognition/calibrate.py
@@ -3,6 +3,8 @@ from chesster.camera.realsense import RealSenseCamera
 import os
 import time
 from pathlib import Path
+import cv2 as cv
+import numpy as np
 
 camera = RealSenseCamera()
 camera.capture_color()
@@ -10,6 +12,7 @@ camera.capture_depth()
 time.sleep(1)
 c_img = camera.capture_color()
 d_img, _ = camera.capture_depth()
+
 board = ObjectRecognition.create_chessboard_data(c_img, d_img, Path('chessboard_data.pkl'), debug=True)
 
 print('')
